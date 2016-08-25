024677550dac9c0f3b72d8b59a66e0df8099a75a
fjohno <ohno.yuri@csis.u-tokyo.ac.jp>
2016-08-25 23:25:27 +0900

    modify tf task.

--- tasks/tensorflow_task/workspace/model.py
+++ tasks/tensorflow_task/workspace/model.py
@@ -1,172 +1,64 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Simple, end-to-end, LeNet-5-like convolutional MNIST model example.
-
-This should achieve a test error of 0.7%. Please keep this model as simple and
-linear as possible, it is meant as a tutorial for simple convolutional models.
-Run with --self_test on the command line to execute a short self-test.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import gzip
-import os
-import sys
-import time
-
-import numpy
-from six.moves import urllib
-from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 
-def inference(images, train=False):
-    """The Lenet-like model
-    Args:
-        images: Images placeholder, from input()
+def inference(images):
+    # conv1
 
-    Returns:
-        logits: output logits
-    """
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
 
-    # The variables below hold all the trainable weights. They are passed an
-    # initial value which will be assigned when we call:
-    # {tf.initialize_all_variables().run()}
-    with tf.name_scope('conv1'):
-        conv1_weights = tf.Variable(
-            tf.truncated_normal([5, 5, NUM_CHANNELS, 32], # 5x5 filter, depth 32.
-                                stddev=0.1,
-                                seed=SEED, dtype=data_type()))
-        conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
-        # 2D convolution, with 'SAME' padding (i.e. the output feature map has
-        # the same size as the input). Note that {strides} is a 4D array whose
-        # shape matches the data layout: [image index, y, x, depth].
-        conv = tf.nn.conv2d(data,
-                            conv1_weights,
-                            strides=[1, 1, 1, 1],
-                            padding='SAME')
-        # Bias and rectified linear non-linearity.
-        relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+    x_image = tf.reshape(images, [-1, 28, 28, 1])
 
-        # Max pooling. The kernel size spec {ksize} also follows the layout of
-        # the data. Here we have a pooling window of 2, and a stride of 2.
-        pool = tf.nn.max_pool(relu,
-                              ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1],
-                              padding='SAME')
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
     # conv2
-    with tf.variable_scope('conv2'):
-        conv2_weights = tf.Variable(tf.truncated_normal(
-            [5, 5, 32, 64], stddev=0.1,
-            seed=SEED, dtype=data_type()))
-        conv2_biases = tf.Variable(
-                           tf.constant(0.1, shape=[64], dtype=data_type()))
-        conv = tf.nn.conv2d(pool,
-                            conv2_weights,
-                            strides=[1, 1, 1, 1],
-                            padding='SAME')
-        relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
-        pool = tf.nn.max_pool(relu,
-                              ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1],
-                              padding='SAME')
 
-        # Reshape the feature map cuboid into a 2D matrix to feed it to the
-        # fully connected layers.
-        pool_shape = pool.get_shape().as_list()
-        reshape = tf.reshape(
-            pool,
-            [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
 
-        fc1_weights = tf.Variable(  # fully connected, depth 512.
-             tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
-                                 stddev=0.1,
-                                 seed=SEED,
-                                 dtype=data_type()))
-        fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
-        # Fully connected layer. Note that the '+' operation automatically
-        # broadcasts the biases.
-        hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+    # fc
 
-        fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
-                                                      stddev=0.1,
-                                                      seed=SEED,
-                                                      dtype=data_type()))
-        fc2_biases = tf.Variable(tf.constant(
-             0.1, shape=[NUM_LABELS], dtype=data_type()))
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
 
-        # Add a 50% dropout during training only. Dropout also scales
-        # activations such that no rescaling is needed at evaluation time.
-        if train:
-            hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
-        return tf.matmul(hidden, fc2_weights) + fc2_biases
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+
+    return tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2), keep_prob
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
 
 
 def loss(logits, labels):
-    """Add L2Loss to all the trainable variables.
-    Add summary for "Loss" and "Loss/avg".
-    Args:
-        logits: Logits from inference().
-        labels: Labels from distorted_inputs or inputs(). 1-D tensor
-                of shape [batch_size]
-    Returns:
-    Loss tensor of type float.
-    """
-
-    # Training computation: logits + cross-entropy loss.
-    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits, labels))
-
-    # L2 regularization for the fully connected parameters.
-    regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
-                    tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
-
-    # Add the regularization term to the loss.
-    loss += 5e-4 * regularizers
-
-    return loss
+    return -tf.reduce_sum(labels*tf.log(logits))
 
 
 def training(loss):
-    """Sets up the training Ops.
-    Creates a summarizer to track the loss over time in TensorBoard.
-    Creates an optimizer and applies the gradients to all trainable variables.
-    The Op returned by this function is what must be passed to the
-    `sess.run()` call to cause the model to train.
-    Args:
-        loss: Loss tensor, from loss().
-        learning_rate: The learning rate to use for gradient descent.
-    Returns:
-        train_op: The Op for training.
-    """
-    # Optimizer: set up a variable that's incremented once per batch and
-    # controls the learning rate decay.
-    batch = tf.Variable(0)
-    # Decay once per epoch, using an exponential schedule starting at 0.01.
-    learning_rate = tf.train.exponential_decay(
-                                              0.01,  # Base learning rate.
-                                              batch * BATCH_SIZE,  # Current index into the dataset.
-                                              train_size,          # Decay step.
-                                              0.95,                # Decay rate.
-                                              staircase=True)
-    # Use simple momentum for the optimization.
-    optimizer = tf.train.MomentumOptimizer(learning_rate,
-                                         0.9).minimize(loss,
-                                                       global_step=batch)
-    return optimizer
+    return tf.train.AdamOptimizer(1e-4).minimize(loss)
